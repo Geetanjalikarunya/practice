da7233c60191180c6e5a3b38f2e664e8e787304b
Geetanjali733 <geetanjalim733@gmail.com>
2023-09-04 10:52:37 +0530

    First commit

---
+++ src/test/java/sample/test1.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void test1() {
+		Reporter.log("Test1");
+	}
+	
+	@Test
+	public void test2() {
+		Reporter.log("Test2");
+	}
+}
